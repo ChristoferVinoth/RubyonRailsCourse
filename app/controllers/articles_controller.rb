bb3578693c291f2116737d810b7a931c7f107257
Vinoth Christofer <vinoth.christofer@freshworks.com>
2017-12-12 12:04:55 +0530

    done show and edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,14 +3,32 @@ def new
   @article = Article.new
 end
 
+def edit
+  @article = Article.find(params[:id])
+end
+
 def create
 #  render plain: params[:article].inspect
   @article = Article.new(article_params)
   if @article.save
     flash[:notice] = "Article is created successfully!"
-    redirect_to articles_path(@article)
+    redirect_to article_path(@article)
   else
     render 'new'
+  end
+end
+
+def index
+
+end
+
+def update
+  @article = Article.find(params[:id])
+  if @article.update(article_params)
+    flash[:notice] = "Article is successfully updated"
+    redirect_to article_path(@article)
+  else
+    render 'edit'
   end
 end
 

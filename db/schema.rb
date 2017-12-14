ce67215d30229504ab749f1794b59b4a1777b82a
Vinoth Christofer <vinoth.christofer@freshworks.com>
2017-12-14 12:06:52 +0530

    add password to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171214051637) do
+ActiveRecord::Schema.define(version: 20171214063255) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20171214051637) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
